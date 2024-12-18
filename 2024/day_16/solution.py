0dba3fb8c2a33df08db6dba3fa0b3584d0611106
Konstantin Ermachkov <konstantin.ermachkov@cetitec.com>
2024-12-18 12:30:08 +0100

    Update 2024 day 16 solution 1

--- 2024/day_16/solution.py
+++ 2024/day_16/solution.py
@@ -24,17 +24,18 @@ with open(f"{dir_path}/input.txt", "r") as file:
     max_y = y
     max_x = x
 
-def deer_move(position: tuple, visited: set) -> list:
+max_v = max_y * max_x - len(walls)
+
+def deer_moves(position: tuple) -> list:
     res = []
-    y, x = position[0]
+    y, x = position
     moves = [(y + dy, x + dx) for dy, dx in [(0, 1), (0, -1), (1, 0), (-1, 0)]]
     for m in moves:
-        if (m not in walls) and (m not in visited):
+        if (m not in walls):
             res.append(m)
     return res
 
-def count_points(position: tuple, next_direction: tuple, total: int) -> int:
-    direction = position[1]
+def count_points(direction: tuple, next_direction: tuple, total: int) -> int:
     if direction == next_direction:
         total += 1
         return total
@@ -42,59 +43,37 @@ def count_points(position: tuple, next_direction: tuple, total: int) -> int:
         total += 1001
         return total
 
-routes = []
-routes.append([start, 0, set()])
-route = routes.pop()
-pos = route[0]
-total = route[1]
-vis = route[2]
-dm = deer_move(pos, vis)
-for move in dm:
-    vis.add(pos[0])
-    routes.append([pos, total, vis])
+visited = set()
+spt = {}
+spt[start[0]] = [start[0], start[1], 0]
 
-results = []
-
-shortest_paths = set()
-shortest_paths.add((start[0], 0))
-
-def find_routes(routes: list) -> list:
-    res = []
-    for route in routes:
-        pos = route[0]
-        total = route[1]
-        vis = route[2]
-        for path in shortest_paths.copy():
-            if pos[0] in path:
-                p = min(total, path[1])
-                break
+v = 0
+while v < len(spt):
+    v = 0
+    for position in spt.copy():
+        direction = spt[position][1]
+        distance = spt[position][2]
+        if position == end:
+            v += 1
+            continue
+        if (position in visited):
+            v += 1
+        visited.add(position)
+        moves = deer_moves(position)
+        if not moves:
+            v += 1
+        for move in moves:
+            y, x = position
+            ny, nx = move
+            nd = ny - y, nx - x
+            t = count_points(direction, nd, distance)
+            if move in spt:
+                t2 = spt[move][2]
+                if t < t2:
+                    spt[move][0] = position
+                    spt[move][1] = nd
+                    spt[move][2] = t
             else:
-                p = total
-                shortest_paths.add((pos[0], total))
-        if p < total:
-            continue
-        if results and total > min(results):
-            continue
-        dm = deer_move(pos, vis)
-        if end in vis:
-            results.append(total)
-            continue
-        if dm:
-            for move in dm:
-                ny, nx = move
-                y, x = pos[0]
-                nd = ny - y, nx - x
-                t = count_points(pos, nd, total)
-                next_pos = (move, nd)
-                v = vis.copy()
-                v.add(move)
-                item = [next_pos, t, v]
-                if item not in res:
-                    res.append(item)
-    return res
+                spt[move] = [position, nd, t]
 
-while routes:
-    routes = find_routes(routes)
-
-if results:
-    print(min(results))
+print(spt[end][2])
