e5fc430539e663254f1a1c738c2f382ff6a6f64d
Konstantin Ermachkov <kermachkov@gmail.com>
2024-12-19 18:11:35 +0100

    Add 2024 day 19 solution 1

---
+++ 2024/day_19/solution.py
@@ -0,0 +1,48 @@
+import os
+
+towels = []
+designs = []
+
+dir_path = os.path.dirname(os.path.realpath(__file__))
+
+with open(f"{dir_path}/input.txt", "r") as file:
+    chunks = file.read().strip().split("\n\n")
+    towels = chunks[0].strip().split(", ")
+    designs = chunks[1].strip().split("\n")
+
+# test_towels = ["r", "wr", "b", "g", "bwu", "rb", "gb", "br"]
+# test_designs = ["brwrr", "bggr", "gbbr", "rrbgbr", "ubwu", "bwurrg", "brgr", "bbrgwb"]
+
+def find_match(design: str) -> bool:
+    matches = []
+    for t in towels:
+        d = design
+        if d.startswith(t):
+            d = d[len(t):]
+            matches.append([d, t])
+    while matches:
+        tmp_m = []
+        for m in range(len(matches)):
+            m = matches.pop()
+            des = m[0]
+            res = m[1]
+            if res == design:
+                return True
+            for t in towels:
+                if des.startswith(t):
+                    d = des[len(t):]
+                    r = res + t
+                    item = [d, r]
+                    if item not in tmp_m:
+                        tmp_m.append([d, r])
+        matches = tmp_m
+    if not matches:
+        return False
+
+total = 0
+for design in designs:
+    fm = find_match(design)
+    if fm:
+        total += 1
+
+print(total)
