93535b5b5506f0d91711228ae054f39b63a4e785
Konstantin Ermachkov <kermachkov@gmail.com>
2025-01-03 13:04:08 +0100

    Update 2024 day 17 solution 2

--- 2024/day_17/solution2.py
+++ 2024/day_17/solution2.py
@@ -50,9 +50,8 @@ def proccess_program(a, b, c: int, pr: list) -> list:
         elif opcode == 5:
             out = get_combo(operand) % 8
             result.append(out)
-            if len(result) == len(program):
-                if result[-1] == program[-1]:
-                    print(a)
+            # if program[:len(result)] != result:
+            #     break
             i += 2
         elif opcode == 6:
             b = a // 2**get_combo(operand)
@@ -61,3 +60,25 @@ def proccess_program(a, b, c: int, pr: list) -> list:
             c = a // 2**get_combo(operand)
             i += 2
     return result
+
+# Program: 2,4,1,3,7,5,0,3,1,5,4,4,5,5,3,0
+# 1. B = A % 8              Always [0..7]
+# 2. B = B ^ 3              Always [0..7]
+# 3. C = A // 2**B          [0, 3]
+# 4. A = A // 8             [0, [1..7],  ]
+# 5. B = B ^ B              always B = 0
+# 6. B = B ^ C              [0, 3]
+# 7. res = B % 8            [0, 3, 5, 5, 4, 4, 5, 1, 3, 0, 5, 7, 3, 1, 4, 2]
+# 8. Go to 1. or exit
+
+end = 8 ** 16
+A = 1
+
+while A < end:
+    o = proccess_program(A, 0, 0, program)
+    if o == program:
+        print("Initial A: ", A)
+        break
+    if o == program[-len(o):]:
+        A = A << 3
+    A += 1
