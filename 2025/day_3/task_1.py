6932cf752742f3c4d899fa34888462fa582f255b
kermachkov-agi <ext.kermachkov@arisglobal.com>
2025-12-03 09:25:57 +0100

    fix 2025 day 3 task 1

--- 2025/day_3/task_1.py
+++ 2025/day_3/task_1.py
@@ -6,20 +6,18 @@ with open(f'{dir_path}/input.txt', 'r') as file:
     res = []
     batteries = [line.strip() for line in file.readlines()]
     for battery in batteries:
-        start = battery[:2]
-        current_max = int(start)
-        for i in range(2, len(battery)):
-            number = battery[i]
-            if (i <= (len(battery) - 2)):
-                a = int(battery[i:i+2])
-                if a > current_max:
-                    current_max = a
-                    start = str(a)
-                    continue
-            n = int(start[0] + number)
-            if n > current_max:
-                current_max = n
+        first, second = battery[:2]
+        current_max = int(first + second)
+        for i in range(1, len(battery)):
+            n = int(battery[i])
+            if (n > int(first)) and (i < len(battery) - 1):
+                first = str(n)
+                second = battery[i+1]
+                current_max = int(first+second)
+            elif n > int(second):
+                second = str(n)
+                current_max = int(first+second)
         res.append(current_max)
-        print(current_max, battery)
+
     print(sum(res))
 
