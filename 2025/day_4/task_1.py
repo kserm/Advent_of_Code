b647167c92c9a32f02bed48ca20cb2ab12b10a20
Konstantin Ermachkov <kermachkov@gmail.com>
2025-12-04 11:32:25 +0100

    add 2025 day 4 task 1 solution

---
+++ 2025/day_4/task_1.py
@@ -0,0 +1,27 @@
+import os
+
+dir_path = os.path.dirname(os.path.realpath(__file__))
+
+
+def check_postition(data, y, x, my, mx):
+    counter = 0
+    positions = [(-1, 0), (-1, -1), (-1, 1), (0, -1), (0, 1), (1, 0), (1, -1), (1, 1)]
+    for p in positions:
+        ny, nx = p[0] + y, p[1] + x
+        if (0 <= ny < my) and (0 <= nx < mx):
+            if data[ny][nx] == "@":
+                counter += 1
+    return counter
+
+
+with open(f"{dir_path}/input.txt", "r") as file:
+    rolls = [list(line.strip()) for line in file.readlines()]
+    max_y, max_x = len(rolls), len(rolls[0])
+    res = set()
+    for y in range(max_y):
+        for x in range(max_x):
+            if rolls[y][x] == "@":
+                r = check_postition(rolls, y, x, max_y, max_x)
+                if r < 4:
+                    res.add((y, x))
+    print(len(res))
